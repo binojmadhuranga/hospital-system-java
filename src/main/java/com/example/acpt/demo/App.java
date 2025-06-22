a85c016cecf0df50edfbb5e1d8fc9b467fa3818e
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-06-22 10:03:04 +0530

    Create artifactor

---
+++ src/main/java/com/example/acpt/demo/App.java
@@ -0,0 +1,12 @@
+package com.example.acpt.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Launching JavaFX Application...");
+        HelloApplication.main(args);
+
+    }
+
+}
